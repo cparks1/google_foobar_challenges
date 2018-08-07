64607ef02b98442032012e1e808e22e70458e7ce
Christopher <cparks13@live.com>
2018-08-06 20:44:06 -0400

    Implemented minimal solution utilizing a character map.

--- level_1.py
+++ level_1.py
@@ -1,55 +1,49 @@
 """
 I Love Lance & Janice
 =====================
-You've caught two of your fellow minions passing coded notes back and forth - while they're on duty, no less! Worse, you're pretty sure it's not job-related - they're both huge fans of the space soap opera "Lance & Janice". You know how much Commander Lambda hates waste, so if you can prove that these minions are wasting her time passing non-job-related notes, it'll put you that much closer to a promotion.
-Fortunately for you, the minions aren't exactly advanced cryptographers. In their code, every lowercase letter [a..z] is replaced with the corresponding one in [z..a], while every other character (including uppercase letters and punctuation) is left untouched.  That is, 'a' becomes 'z', 'b' becomes 'y', 'c' becomes 'x', etc.  For instance, the word "vmxibkgrlm", when decoded, would become "encryption".
+You've caught two of your fellow minions passing coded notes back and forth - while they're on duty, no less!
+Worse, you're pretty sure it's not job-related - they're both huge fans of the space soap opera "Lance & Janice".
+You know how much Commander Lambda hates waste, so if you can prove that these minions
+are wasting her time passing non-job-related notes, it'll put you that much closer to a promotion.
+
+Fortunately for you, the minions aren't exactly advanced cryptographers.
+In their code, every lowercase letter [a..z] is replaced with the corresponding one in [z..a],
+while every other character (including uppercase letters and punctuation) is left untouched.
+That is, 'a' becomes 'z', 'b' becomes 'y', 'c' becomes 'x', etc.  For instance, the word "vmxibkgrlm", when decoded, would become "encryption".
 Write a function called answer(s) which takes in a string and returns the deciphered string so you can show the commander proof that these minions are talking about "Lance & Janice" instead of doing their jobs.
-
-Languages
-=========
-To provide a Python solution, edit solution.py
-To provide a Java solution, edit solution.java
-
-Test cases
-==========
-Inputs:
-    (string) s = "wrw blf hvv ozhg mrtsg'h vkrhlwv?"
-Output:
-    (string) "did you see last night's episode?"
-
-Inputs:
-    (string) s = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"
-Output:
-    (string) "Yeah! I can't believe Lance lost his job at the colony!!"
 """
 
+# Dictionary mapping each letter to its "opposite" character on the alphabet.
+# I used the following python code to generate the dictionary, and then just printed it and pasted it into the solution,
+# as running the code once to generate a dictionary that will be used multiple times is better than generating it
+# on every import.
+# I figured Cmd. Lambda would enjoy the minimal solution.
 
-def decode(char):
-    char_ascii = ord(char)
+# Generation code:
+# from string import ascii_uppercase, ascii_lowercase
+# opposites_map = {}
+# for i, char in enumerate(ascii_lowercase):
+#   opposites_map[char] = ascii_lowercase[-i+1]
+# for char, opposite in opposites_map.iteritems():
+#   print("'%s': '%s,'" % (char, opposite))
 
-    # Define shifting boundaries. Minions are only shifting letters, not punctuation or white space.
-    min_lower = ord('a')  # a (ascii) = 97
-    max_lower = ord('z')  # z (ascii) = 122
-    min_upper = ord('A')  # A (ascii) = 65
-    max_upper = ord('Z')  # Z (ascii) = 90
+# I then took the output and removed the extra comma at the end,
+# placed the brackets around the data, and formatted it a little.
 
-    shift = 26  # The minions are shifting letters by 26. A=Z, Z=A, ...
-
-    new_ord = char_ascii + shift - 1
-    if min_lower <= char_ascii <= max_lower:
-        if new_ord > max_lower:
-            new_ord = min_lower + (new_ord - max_lower)
-    elif min_upper <= char_ascii <= max_upper:
-        if new_ord > max_upper:
-            new_ord = min_upper + (new_ord - max_upper)
-    else:
-        return char
-
-    return chr(new_ord)
+opposites_map = {
+    'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q', 'k': 'p', 'l': 'o', 'm': 'n',
+    'n': 'm', 'o': 'l', 'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g', 'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a'
+}
 
 
 def answer(s):
+    """
+    Decodes a given string, based on the parameters given.
+    Those parameters being that only lowercase characters are shifted, and that uppercase and punctuation is untouched.
+    :param s: String to decode.
+    :return:  Decoded string.
+    """
     ans = ''
     for char in s:  # Iterate through each character
-        ans += decode(char)
+        ans += opposites_map[char] if char in opposites_map else char
     return ans
--- test_answers.py
+++ test_answers.py
@@ -2,6 +2,21 @@ from unittest import TestCase
 import level_1
 
 
+"""
+Level 1 Test cases
+==========
+Inputs:
+    (string) s = "wrw blf hvv ozhg mrtsg'h vkrhlwv?"
+Output:
+    (string) "did you see last night's episode?"
+
+Inputs:
+    (string) s = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"
+Output:
+    (string) "Yeah! I can't believe Lance lost his job at the colony!!
+"""
+
+
 class TestAnswers(TestCase):
     def test_level1_answer(self):
         test_cases = [("wrw blf hvv ozhg mrtsg'h vkrhlwv?",
